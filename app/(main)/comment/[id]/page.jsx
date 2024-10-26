a9d4878ce7c96be90c78c4ab06e718cd4fc7664e
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-26 22:26:31 +0300

    detailpage css olusturuldu

--- app/(main)/comment/[id]/page.jsx
+++ app/(main)/comment/[id]/page.jsx
@@ -1,6 +1,7 @@
 import Comments from "@/components/comments";
 import EditFeedbackButton from "@/components/editFeedbackButton";
 import GoBack from "@/components/goBack";
+import "../detailPage.css";
 
 export default function pageDetail({ params }) {
   const { id } = params;
