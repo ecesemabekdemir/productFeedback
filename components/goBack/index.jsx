ce26c79353331760f5f057ff929be9a0483af39e
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-26 20:01:38 +0300

    go back componenti olusturuldu.

---
+++ components/goBack/index.jsx
@@ -0,0 +1,11 @@
+export default function GoBack() {
+  return (
+    <>
+      <div className="goBack">
+        <Link href="/" className="goBackButton">
+          Go Back
+        </Link>
+      </div>
+    </>
+  );
+}
