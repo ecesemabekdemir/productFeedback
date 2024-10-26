555be858593958dd6648e244e36bcb072fbedf0e
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-10-27 00:59:45 +0300

    sidebarın mobili eklendi

---
+++ svgs/hamburger/index.jsx
@@ -0,0 +1,9 @@
+export default function Hamburger() {
+  return (
+    <svg width="20" height="17" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="20" height="3" fill="white"/>
+    <rect y="7" width="20" height="3" fill="white"/>
+    <rect y="14" width="20" height="3" fill="white"/>
+    </svg>
+  )
+}
