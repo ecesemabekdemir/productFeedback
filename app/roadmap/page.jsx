cfd80ae28cd3c2a2e4177e4be1d9009dd79b40a8
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-28 12:54:42 +0300

    log silindi sayfada hata veriyordu.

--- app/roadmap/page.jsx
+++ app/roadmap/page.jsx
@@ -2,7 +2,6 @@ import RoadmapComp from "@/components/roadmap";
 import RoadmapHeader from "@/components/roadmap-header";
 
 export default async function RoadmapPage() {
-  console.log({ response });
   return (
     <div className="roadmapContainer">
       <RoadmapHeader />
