567c27448f912d6edfd123af9e0c6d2be8b3fa38
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-29 01:21:41 +0300

    sidebardaki category kısmını yapıldı dummyjason posta göre . gelen api e göre değişikliik gösterebilir.

--- components/sidebarFilter/index.jsx
+++ components/sidebarFilter/index.jsx
@@ -1,45 +1,50 @@
 "use client";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-export default function SidebarFilter({ response }) {
+export default function SidebarFilter({ data }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-
   const router = useRouter();
   const categoryParam = searchParams.get("tags");
 
   const createQueryString = (name, value) => {
     const params = new URLSearchParams(searchParams.toString());
     params.set(name, value);
-
     return params.toString();
   };
+
+  // normal api den category gelmeli data'nın response.altındaki posts dizisini kullandık gercek apiden direkt de gelebilir
+  const tagsArray = data?.response?.posts || [];
 
   return (
     <div className="sidebarFilter">
       <div className="item">
         <button
           onClick={() => router.push(`${pathname}`)}
-          className={`${categoryParam ? "select" : ""}`}
+          className={!categoryParam ? "select" : ""}
         >
           All
         </button>
       </div>
-      <div className="item">
-        {response?.response.slice(0, 5).map((tags, index) => (
-          <button
-            onClick={() => {
-              router.push(
-                `${pathname}?${createQueryString("tags", tags.slug)}`
-              );
-            }}
-            key={index}
-            className={`${categoryParam === tags.slug ? "select" : ""}`}
-          >
-            {tags.name}
-          </button>
-        ))}
-      </div>
+      {tagsArray.length > 0 ? (
+        tagsArray.slice(0, 5).map((tag, index) => (
+          <div className="item">
+            <button
+              onClick={() => {
+                router.push(
+                  `${pathname}?${createQueryString("tags", tag.slug)}`
+                );
+              }}
+              key={index}
+              className={categoryParam === tag.slug ? "select" : ""}
+            >
+              {tag.tags}
+            </button>
+          </div>
+        ))
+      ) : (
+        <p>No tags available.</p>
+      )}
     </div>
   );
 }
