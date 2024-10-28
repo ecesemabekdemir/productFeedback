be93d22f1c8fc4cf4f822056f21ed1d7e5a3a137
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-28 12:25:44 +0300

    feedback.css ve index te düzenlemeler yapıldı.

--- components/feedbackCard/index.jsx
+++ components/feedbackCard/index.jsx
@@ -12,25 +12,29 @@ export default async function FeedbackCard() {
   //onClick={() => Router.push("/comment/1")}
   return (
     <div className="feedbackCardContainer">
-      <div className="feedbackCard">
-        <div className="cardContent">
-          <div className="likes">
-            <p>50</p>
-          </div>
-          <div className="feedBackContent">
-            <div className="feedBackContentItem">
-              {response.posts.map((x) => (
-                <p>{x.title}</p>
-              ))}
+      <>
+        {response.posts.map((x) => (
+          <div className="feedbackCard">
+            <div className="feedBackContent">
+              <div className="cardContent">
+                <div className="likes">
+                  <p>{x.reactions.likes}</p>
+                </div>
+                <div className="feedBackContentItem">
+                  <div>
+                    <h3>{x.title}</h3>
+                    <p>içerik gelicek</p>
+                    <p className="tag">Enhancement</p>
+                  </div>
+                </div>
+              </div>
             </div>
-
-            <p className="tag">Enhancement</p>
+            <div className="comment">
+              <CommentIcon />
+            </div>
           </div>
-        </div>
-        <div className="comment">
-          <CommentIcon />
-        </div>
-      </div>
+        ))}
+      </>
     </div>
   );
 }
