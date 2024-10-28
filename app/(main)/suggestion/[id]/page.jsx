34151dfc94c3b3f860024634b4e7dfd0bb2ddbb0
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-28 12:55:51 +0300

    comments componentini suggestion-item a çevirdim. her bir suggestion için postuna ve comment ine gideceği için. global css de a etiketine style kaldırdım.

--- app/(main)/suggestion/[id]/page.jsx
+++ app/(main)/suggestion/[id]/page.jsx
@@ -1,8 +1,9 @@
 "use client";
-import Comments from "@/components/comments";
+
 import EditFeedbackButton from "@/components/editFeedbackButton";
 import GoBack from "@/components/goBack";
 import "../detailPage.css";
+import SuggestionItem from "@/components/suggestion-item";
 
 export default function PageDetail({ params }) {
   const { id } = params;
@@ -14,7 +15,7 @@ export default function PageDetail({ params }) {
           <GoBack />
           <EditFeedbackButton />
         </div>
-        <Comments />
+        <SuggestionItem />
       </div>
     </>
   );
