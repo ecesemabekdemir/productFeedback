d1057de9ddf0b121f0e936c59f75f4d4b6761048
Doğa Savaş <dogasvs@Doga-MacBook-Air.local>
2024-11-09 01:02:16 +0300

    categorye göre filtreleme yapıldı

--- components/feedbackCard/index.jsx
+++ components/feedbackCard/index.jsx
@@ -2,36 +2,44 @@ import CommentIcon from "@/svgs/comment";
 import "./feedback.css";
 import Link from "next/link";
 import UpdownIcon from "@/svgs/updownicon";
+import Empty from "../empty";
 
-export default function FeedbackCard({ feedbackData }) {
+export default function FeedbackCard({ feedbackData, category }) {
+  console.log(feedbackData.response)
+  let filterData = category ? feedbackData.response.filter(x => x.name == category) : feedbackData.response
+
   return (
     <div id="feedback" className="feedbackCardContainer">
-      {feedbackData.response.map((x, i) => (
-        <div key={i} className="feedbackCard">
-          <div className="feedBackContent">
-            <div className="cardContent">
-              <div className="likes">
-                <UpdownIcon />
-                <p>{x.uploadsCount}</p>
-              </div>
-              <Link
-                href={`/suggestion/${x.id}`}
-                className="feedBackContentItem"
-              >
-                <div>
-                  <h3>{x.title}</h3>
-                  <p>{x.description}</p>
-                  <p className="tag">{x.name}</p>
+      {filterData.length > 0 ? 
+        filterData.map((x, i) => (
+          <div key={i} className="feedbackCard">
+            <div className="feedBackContent">
+              <div className="cardContent">
+                <div className="likes">
+                  <UpdownIcon />
+                  <p>{x.uploadsCount}</p>
                 </div>
-              </Link>
+                <Link
+                  href={`/suggestion/${x.id}`}
+                  className="feedBackContentItem"
+                >
+                  <div>
+                    <h3>{x.title}</h3>
+                    <p>{x.description}</p>
+                    <p className="tag">{x.name}</p>
+                  </div>
+                </Link>
+              </div>
+            </div>
+            <div className="comment">
+              <CommentIcon />
+              <p>{x.commit?.length}</p>
             </div>
           </div>
-          <div className="comment">
-            <CommentIcon />
-            <p>{x.commit?.length}</p>
-          </div>
-        </div>
-      ))}
+        ))
+      : 
+        <Empty />
+      }
     </div>
   );
 }
