0c74e8ce3320f52d420d924c0085bbf44ec0da0f
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-11-09 02:08:48 +0300

    hata düzeltildi

--- components/commentList/index.jsx
+++ components/commentList/index.jsx
@@ -5,11 +5,9 @@ import "./commentList.css";
 export default function CommentList({ feedbackDetail }) {
   return (
     <div className="commentListContainer">
+      <h2>{feedbackDetail?.commits?.length} Comment</h2>
       {feedbackDetail?.commits?.map((comment, i) => (
-        <div key={i}>
-          <h2>{feedbackDetail?.commits?.length} Comment</h2>
-          <CommentCard comment={comment} />
-        </div>
+        <CommentCard key={i} comment={comment} />
       ))}
     </div>
   );
